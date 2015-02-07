da889bfb6d8413e83c5a169a63490f290c37c668
Matt Cooper <matthew_cooper@brown.edu>
2015-02-07 16:23:02 -0500

    Created the project

---
+++ hackatbrown-2015/hackatbrown-2015Tests/hackatbrown_2015Tests.m
@@ -0,0 +1,40 @@
+//
+//  hackatbrown_2015Tests.m
+//  hackatbrown-2015Tests
+//
+//  Created by Matt Cooper on 2/7/15.
+//  Copyright (c) 2015 Matthew Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface hackatbrown_2015Tests : XCTestCase
+
+@end
+
+@implementation hackatbrown_2015Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
