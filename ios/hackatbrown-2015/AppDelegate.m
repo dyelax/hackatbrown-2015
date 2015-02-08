2e7d1da88d4be13a4274a54272a6845c39a4eb54
rmiyagi511 <rick_miyagi@brown.edu>
2015-02-07 19:25:22 -0500

    Spotify login

--- ios/hackatbrown-2015/AppDelegate.m
+++ ios/hackatbrown-2015/AppDelegate.m
@@ -7,18 +7,70 @@
 //
 
 #import "AppDelegate.h"
+#import <Spotify/Spotify.h>
+#import "ENAPI.h"
+
+static NSString * const kClientId = @"b72555a696a64d42b465d778d52825ec";
+static NSString * const kCallbackURL = @"hackatBrown://callback";
+static NSString * const kTokenSwapURL = @"http://localhost:1234/swap";
 
 @interface AppDelegate ()
+
+@property (nonatomic, strong) SPTSession *session;
+@property (nonatomic, strong) SPTAudioStreamingController *player;
 
 @end
 
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // Create SPTAuth instance; create login URL and open it
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    NSURL *loginURL = [auth loginURLForClientId:kClientId
+                            declaredRedirectURL:[NSURL URLWithString:kCallbackURL]
+                                         scopes:@[SPTAuthStreamingScope]];
+    
+    // Opening a URL in Safari close to application launch may trigger
+    // an iOS bug, so we wait a bit before doing so.
+    [application performSelector:@selector(openURL:)
+                      withObject:loginURL afterDelay:0.1];
+    
     return YES;
 }
+
+// Handle auth callback
+-(BOOL)application:(UIApplication *)application
+           openURL:(NSURL *)url
+ sourceApplication:(NSString *)sourceApplication
+        annotation:(id)annotation {
+    
+    // Ask SPTAuth if the URL given is a Spotify authentication callback
+    if ([[SPTAuth defaultInstance] canHandleURL:url withDeclaredRedirectURL:[NSURL URLWithString:kCallbackURL]]) {
+        
+        // Call the token swap service to get a logged in session
+        [[SPTAuth defaultInstance]
+         handleAuthCallbackWithTriggeredAuthURL:url
+         tokenSwapServiceEndpointAtURL:[NSURL URLWithString:kTokenSwapURL]
+         callback:^(NSError *error, SPTSession *session) {
+             
+             if (error != nil) {
+                 NSLog(@"*** Auth error: %@", error);
+                 return;
+             }
+             
+             // Call the -playUsingSession: method to play a track
+             
+         }];
+        return YES;
+    }
+    
+    return NO;
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
